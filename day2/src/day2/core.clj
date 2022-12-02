b99466d9ce2d463e0d602d1eecbc446b49fb32f7
Matthew Lemon <matt@matthewlemon.com>
2022-12-02 21:43:32 +0000

    problems

---
+++ day2/src/day2/core.clj
@@ -0,0 +1,60 @@
+(ns day2.core
+  (:require [clojure.string :as str]))
+
+;; Reference
+;; ---------
+;; theirs  mine value  type
+;; A       X    1      Rock
+;; B       Y    2      Paper
+;; C       Z    3      Scissors
+
+;; Round scores
+;; 0 loss
+;; 3 draw
+;; 6 win
+
+(def pairs (map vector (str/split-lines(slurp "input.txt"))))
+
+(defn game [theirs ours]
+  (cond
+    (and (= theirs "A") (= ours "Y")) :win
+    (and (= theirs "A") (= ours "Z")) :loss
+    (and (= theirs "A") (= ours "X")) :draw
+    (and (= theirs "B") (= ours "Y")) :draw
+    (and (= theirs "B") (= ours "Z")) :win
+    (and (= theirs "B") (= ours "X")) :loss
+    (and (= theirs "C") (= ours "Y")) :loss
+    (and (= theirs "C") (= ours "Z")) :draw
+    (and (= theirs "C") (= ours "X")) :win
+    ))
+
+(defn evaluate-result [result]
+  (cond
+    (= result :win) 6
+    (= result :draw) 3
+    (= result :loss) 0))
+
+(defn get-score [choice]
+  (cond
+    (= choice "X") 1
+    (= choice "Y") 2
+    (= choice "Z") 3))
+
+(defn eval-game [[theirs ours]]
+  (let [result (game theirs ours)
+        result-score (evaluate-result result)
+        choice-score (get-score ours)]
+    (+ result-score choice-score)))
+(comment 
+  (eval-game ["A" "Y"])
+  (eval-game ["B" "X"])
+  (eval-game ["C" "X"]))
+
+
+(defn puzzle-1 [v]
+  (prn v)
+  (reduce + (map eval-game v)))
+
+(comment (reduce + (map eval-game [["A" "Y"] ["B" "X"]])))
+
+(puzzle-1 pairs)
---
+++ day2/test/day2/core_test.clj
@@ -0,0 +1,30 @@
+(ns day2.core-test
+  (:require [clojure.test :refer :all]
+            [day2.core :refer :all]))
+
+(deftest game-win-loss
+  (testing "Paper rock scissors tests"
+    (is (= (game "A" "Y") :win))
+    (is (= (game "A" "Z") :loss))
+    (is (= (game "A" "X") :draw))
+    (is (= (game "B" "Y") :draw))
+    (is (= (game "B" "Z") :win))
+    (is (= (game "B" "X") :loss))
+    (is (= (game "C" "Y") :loss))
+    (is (= (game "C" "Z") :draw))
+    (is (= (game "C" "X") :win))
+    )
+  )
+
+(deftest eval-result
+  (testing "Assign a score to a result"
+    (is (= (evaluate-result :win) 6))
+    (is (= (evaluate-result :draw) 3))
+    (is (= (evaluate-result :loss) 0))))
+
+(deftest my-choice-score
+  (testing "Testing getting a score from my choice"
+    (is (= (get-score "X") 1))
+    (is (= (get-score "Y") 2))
+    (is (= (get-score "Z") 3))
+    ))
