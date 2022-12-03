9350d55fda408390e5d0ed1d5ee56d929592a8fa
Matthew Lemon <matt@matthewlemon.com>
2022-12-03 09:13:26 +0000

    quick fix

--- day2/test/day2/core_test.clj
+++ day2/test/day2/core_test.clj
@@ -37,3 +37,4 @@
 (deftest test-puzzle-1
   (testing "The final solution"
     (is (= (puzzle-1 (lazy-seq ["A" "Y" ["A" "Y"]] ["A" "Y"])) 24))))
+    (is (= (puzzle-1 (lazy-seq ["A" "Y"] ["A" "Y"] ["A" "Y"])) 24))))
