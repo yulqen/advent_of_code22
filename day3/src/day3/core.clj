522b9eab1d188a88c68f232470d62f7479c69d32
Matthew Lemon <matt@matthewlemon.com>
2022-12-03 20:45:12 +0000

    part 1 of day 3 complete

--- day3/src/day3/core.clj
+++ day3/src/day3/core.clj
@@ -1,18 +1,35 @@
 (ns day3.core
   (:require
    [clojure.string :as str]
-   [clojure.data :as data]))
+   [clojure.set :as set]))
 
 (def rucksacks (str/split-lines (slurp "input.txt")))
 
 (defn first-comp [s]
-    [(subs s 0 (/ (count s) 2))])
+    (subs s 0 (/ (count s) 2)))
 
 (defn second-comp [s]
-  [(subs s (/ (count s) 2))])
+  (subs s (/ (count s) 2)))
 
 (defn split-string [s]
   [(first-comp s) (second-comp s)])
 
-(def t (map #(split-string %) rucksacks))
+(def pairs (map #(split-string %) rucksacks))
+pairs
+(defn item-intersect [set1 set2]
+  (set/intersection set1 set2))
+
+(defn comp-compartments [[s1 s2]]
+  (first (item-intersect (set s1) (set s2))))
+
+(defn char-val [char]
+  (if (re-matches #"[a-z]" (str char)) (- (int char) 96) (- (int char) 38)))
+
+(defn puzzle-1 [split-strings]
+  (let [shared-chars (map #(comp-compartments %) split-strings)
+        char-vals (map #(char-val %) shared-chars)]
+    (reduce + 0 char-vals)))
+
+(puzzle-1 pairs)
+
 
--- day3/test/day3/core_test.clj
+++ day3/test/day3/core_test.clj
@@ -1,7 +1,23 @@
 (ns day3.core-test
   (:require [clojure.test :refer :all]
-            [day3.core :refer :all]))
+            [day3.core :refer :all]
+            [clojure.set :as set]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-intersect
+  (testing "Get the intersection between two sets"
+    (is (= #{\d} (item-intersect (set "abcd") (set "defg"))))))
+
+(deftest test-diff-compartment-contents
+  (testing "Get difference between compartments in bag"
+    (is (= #{\h} (comp-compartments ["Cjh" "TTh"])))))
+
+(deftest test-split-str-in-half
+  (testing "Can split even string to vector of two strings"
+    (is (= ["AAA" "BBB"] (split-string "AAABBB")))))
+
+(deftest test-get-char-numeric
+  (testing "Returns the number from a char"
+    (is (= 1 (char-val \a)))
+    (is (= 2 (char-val \b)))
+    (is (= 27 (char-val \A)))))
+
