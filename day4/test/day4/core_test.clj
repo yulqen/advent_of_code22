71158ced7ef66693cd8a38dd1eaaeffbab0ec80c
Matthew Lemon <matt@matthewlemon.com>
2022-12-12 15:16:16 +0000

    Still not passed part 1 of day 4 - using a different algorithm

--- day4/test/day4/core_test.clj
+++ day4/test/day4/core_test.clj
@@ -19,13 +19,29 @@
     (is (= [[1 2 3 4] [3 4 5 6 7]] (parse-pair "1-4,3-7")))))
 
 (deftest set-inside-the-other
+  (is (not (vec-fits [[10 11 12] [7 8 9 10]])))
   (is (vec-fits [[1 2 3 4 5 6] [2 3 4 5]]))
   (is (vec-fits [[3 4] [2 3 4 5]]))
-  (is (vec-fits [[1] [1 2]]))
+  (is (not (vec-fits [[2002 2003] [2002]])))
+  (is (not (vec-fits [[2002] [2002 2003]])))
+  (is (not (vec-fits [[1] [1]])))
+  (is (not (vec-fits [[201 202 203] [1]])))
+  (is (not (vec-fits [[1] [1 2]])))
+  (is (vec-fits [[100 101 102] [101]]))
+  (is (not (vec-fits [[1] [10 11 12]])))
   (is (not (vec-fits [[8 9 10] [1 2]])))
   (is (vec-fits [[98 99 100 101] [99 100]]))
-  (is (vec-fits [[99 100] [99]]))
+  (is (not (vec-fits [[99 100] [99]])))
   (is (not (vec-fits [[98 99] [99 100]])))
   (is (vec-fits [[16 17] [14 15 16 17 18 19]]))
   (is (not (vec-fits [[16 17] [16 17]])))
   (is (not (vec-fits [[16 17] [23 23 24]]))))
+
+;; if they are equal length - return false
+;; else
+;; get length of each vec
+;; IF first of shortest vec is greater than or equal to the first of longest
+;; AND last of shortest vec is less than the last of the longest
+;; return TRUE
+;; else return FALSE
+
